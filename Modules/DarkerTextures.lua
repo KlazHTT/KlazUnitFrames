c9e8da8233c8e3c808c9120d70683a44611bf0d2
KlazHTT <account@haothitran.com>
2024-03-07 22:22:54 -0500

    Add dark unit frames

--- Modules/DarkerTextures.lua
+++ Modules/DarkerTextures.lua
@@ -5,6 +5,31 @@
 local f = CreateFrame('Frame')
 f:RegisterEvent('PLAYER_ENTERING_WORLD')
 f:SetScript('OnEvent', function(self, event)
+
+  for i, v in pairs ({
+    -- player frame
+    PlayerFrame.PlayerFrameContainer.FrameTexture,
+    PlayerFrame.PlayerFrameContainer.AlternatePowerFrameTexture,
+    PlayerFrame.PlayerFrameContainer.VehicleFrameTexture,
+    PlayerCastingBarFrame.Border,
+    PetFrameTexture,
+
+    -- target frame
+    TargetFrame.TargetFrameContainer.FrameTexture,
+    TargetFrameSpellBar.Border,
+    TargetFrameToT.FrameTexture,
+
+    -- focus frame
+    FocusFrame.TargetFrameContainer.FrameTexture,
+    FocusFrameSpellBar.Border,
+    FocusFrameToT.FrameTexture,
+  }) do
+      v:SetVertexColor(0.1, 0.1, 0.1, 1)
+  end
+
+  TargetFrame.TargetFrameContent.TargetFrameContentMain.ReputationColor:Hide()
+  FocusFrame.TargetFrameContent.TargetFrameContentMain.ReputationColor:Hide()
+
   for i, v in pairs ({
     -- paladin holy power
     PaladinPowerBarFrame.Background,    -- no holy power
