ec60bbc3900cd6d3daa6212f3bcda25c12309b44
KlazHTT <account@haothitran.com>
2024-03-09 00:42:14 -0500

    Add hide combat glow on unit frames

--- Modules/Hide.lua
+++ Modules/Hide.lua
@@ -2,9 +2,19 @@
 -- # MODULES > HIDE TEXT
 --------------------------------------------------------------------------------
 
-PlayerFrame:HookScript("OnEvent", function()
+local hiddenFrame = CreateFrame('Frame')
+hiddenFrame:Hide()
+
+PlayerFrame:HookScript('OnEvent', function()
   -- hide player damage and healing received text
   PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.HitText:Hide()
+
+  -- hide combat glow on unit frames
+  PlayerFrame.PlayerFrameContainer.FrameFlash:SetParent(hiddenFrame)
+  TargetFrame.TargetFrameContainer.Flash:SetParent(hiddenFrame)
+  FocusFrame.TargetFrameContainer.Flash:SetParent(hiddenFrame)
+  PetFrameFlash:SetParent(hiddenFrame)
+  PetAttackModeTexture:SetParent(hiddenFrame)
 
   -- hide reputation background
   TargetFrame.TargetFrameContent.TargetFrameContentMain.ReputationColor:Hide()
