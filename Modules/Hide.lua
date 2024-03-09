f26a06e87382575ebbf6e5a5ad90601d50dd0144
KlazHTT <account@haothitran.com>
2024-03-09 01:15:38 -0500

    Add hide rested breathing glow animation

--- Modules/Hide.lua
+++ Modules/Hide.lua
@@ -6,6 +6,8 @@ local hiddenFrame = CreateFrame('Frame')
 hiddenFrame:Hide()
 
 PlayerFrame:HookScript('OnEvent', function()
+  -- hide rested breathing glow
+  PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.StatusTexture:SetParent(hiddenFrame)
   -- hide player damage and healing received text
   PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.HitText:Hide()
 
